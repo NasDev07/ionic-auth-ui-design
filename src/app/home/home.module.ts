bbff7eafa013627801da0f1aaa3dca8a71417f6a
NasDev07 <nasruddin20182019@gmail.com>
2024-04-08 11:54:04 +0700

    Ionic UI Auth

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { HomePageRoutingModule } from './home-routing.module';
+import { AboutComponent } from '../about/about.component';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, AboutComponent],
+})
+export class HomePageModule {}
